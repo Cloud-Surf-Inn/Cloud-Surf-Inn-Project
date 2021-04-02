4e7cd7a4131c4411de091071025bfce8c88252fc
chriskline459 <30421315+chriskline459@users.noreply.github.com>
2021-04-02 14:42:27 -0400

    Add files via upload

---
+++ Demo1Folder/qR.java
@@ -0,0 +1,72 @@
+package qR.SoftwareEng;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import java.util.*;
+
+
+public class qR {
+
+	public static void main(String[] args) throws WriterException, IOException {
+		int length = 10;
+		String qrCodeText = new String(OTP(length));
+		String filePath = "pin.png"; 
+		int size = 125;
+		String fileType = "png";
+		File qrFile = new File(filePath);
+		createQRImage(qrFile, qrCodeText, size, fileType);
+		System.out.println("DONE");
+	}
+	
+	private static char[] OTP(int len){
+
+        String numbers = "0123456789abcdefghijklmnopqrstuvwxyz";
+        Random rndm_method = new Random();
+        char[] otp = new char[len];
+            for (int i = 0; i < len; i++){
+              otp[i] = numbers.charAt(rndm_method.nextInt(numbers.length()));
+            }
+        return otp;
+    }
+
+	private static void createQRImage(File qrFile, String qrCodeText, int size, String fileType)
+			throws WriterException, IOException {
+		// Create the ByteMatrix for the QR-Code that encodes the given String
+		Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<>();
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+		QRCodeWriter qrCodeWriter = new QRCodeWriter();
+		BitMatrix byteMatrix = qrCodeWriter.encode(qrCodeText, BarcodeFormat.QR_CODE, size, size, hintMap);
+		// Make the BufferedImage that are to hold the QRCode
+		int matrixWidth = byteMatrix.getWidth();
+		BufferedImage image = new BufferedImage(matrixWidth, matrixWidth, BufferedImage.TYPE_INT_RGB);
+		image.createGraphics();
+
+		Graphics2D graphics = (Graphics2D) image.getGraphics();
+		graphics.setColor(Color.WHITE);
+		graphics.fillRect(0, 0, matrixWidth, matrixWidth);
+		// Paint and save the image using the ByteMatrix
+		graphics.setColor(Color.BLACK);
+
+		for (int i = 0; i < matrixWidth; i++) {
+			for (int j = 0; j < matrixWidth; j++) {
+				if (byteMatrix.get(i, j)) {
+					graphics.fillRect(i, j, 1, 1);
+				}
+			}
+		}
+		ImageIO.write(image, fileType, qrFile);
+	}
+	
+}
